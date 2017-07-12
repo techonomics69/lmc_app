1c9b807178a5abf91b92b75970838c3c2f46b43b
Luke Jones <lukejones950@hotmail.com>
2017-07-12 23:21:22 +0100

    added static home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@base_title = "Leeds Mountaineering Club"
+	end
+
+	test "should get root" do
+		get '/'
+		assert_response :success
+	end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
-
 end
