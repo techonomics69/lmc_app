9338f29d3e602355784c5f4a1faebc743434247e
Luke Jones <lukejones950@hotmail.com>
2017-07-16 00:03:15 +0100

    basic styling

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,12 +9,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 	test "should get root" do
 		get '/'
 		assert_response :success
-		assert_select "title", "#{@base_title}"
 	end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
     assert_select "title", "#{@base_title}"
+  end
+
+  test "should get help" do
+  	get static_pages_help_url
+  	assert_response :success
+  	assert_select "title", "Help | #{@base_title}"
   end
 end
