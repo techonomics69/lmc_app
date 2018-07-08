a744eeb492280ed3b11f53c1d5deb2b56b9598a1
Luke Jones <lukejones950@hotmail.com>
2018-07-08 13:01:08 +0100

    turn on ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = false
+  config.force_ssl = true
   #config.ssl_options = { redirect: { exclude: -> request { request.path =~ /bb.leedsmc.org/ } } }
   #config.ssl_options = { hsts: { subdomains: false } }
   #http://api.rubyonrails.org/v5.2/classes/ActionDispatch/SSL.html
