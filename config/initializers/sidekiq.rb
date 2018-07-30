977769614784e0af03a27061c292a14bf0a354e9
Luke Jones <lukejones950@hotmail.com>
2018-07-30 22:04:36 +0100

    sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { :size => 9 }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { :size => 1 }
 end
