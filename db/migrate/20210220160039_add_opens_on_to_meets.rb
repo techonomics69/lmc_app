c66b0817aaafa3bd67ac03487337914608c62e33
Luke Jones <lukejones950@hotmail.com>
2021-03-30 23:18:59 +0100

    change migration - remove member_id from meets

--- db/migrate/20210220160039_add_opens_on_to_meets.rb
+++ db/migrate/20210220160039_add_opens_on_to_meets.rb
@@ -1,7 +1,6 @@
 class AddOpensOnToMeets < ActiveRecord::Migration[5.1]
   def change
     add_column :meets, :opens_on, :date
-    # remove_reference :meets, :members, index: true, foreign_key: true
-    # remove_column :meets, :member_id
+    remove_column :meets, :member_id
   end
 end
